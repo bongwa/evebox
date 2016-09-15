35f7356ca31283a53411555ee1a17675f4719e74
Jason Ish <ish@unx.ca>
2016-09-15 17:06:28 -0600

    evereader: handle rotation

--- evereader/evereader.go
+++ evereader/evereader.go
@@ -1,30 +1,82 @@
+/* Copyright (c) 2016 Jason Ish
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package evereader
 
 import (
 	"os"
 	"gopkg.in/square/go-jose.v1/json"
 	"bufio"
+	"bytes"
+	"io"
 )
 
 type EveReader struct {
-	file    *os.File
-	reader  *bufio.Reader
-	decoder *json.Decoder
-	lineno  uint64
+	filename string
+	file     *os.File
+	reader   *bufio.Reader
+	lineno   uint64
+	size     int64
 }
 
 func New(path string) (*EveReader, error) {
 
-	file, err := os.Open(path)
+	eveReader := EveReader{
+		filename: path,
+	}
+	err := eveReader.OpenFile()
 	if err != nil {
 		return nil, err
 	}
 
-	reader := bufio.NewReader(file)
-	decoder := json.NewDecoder(reader)
-	decoder.UseNumber()
+	return &eveReader, nil
+}
 
-	return &EveReader{file: file, reader:reader, decoder:decoder}, nil
+func (er *EveReader) OpenFile() error {
+	file, err := os.Open(er.filename)
+	if err != nil {
+		return err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	er.file = file
+	er.reader = bufio.NewReader(er.file)
+	er.size = fileInfo.Size()
+	er.lineno = 0
+
+	return nil
+}
+
+func (er *EveReader) Reopen() error {
+	er.file.Close()
+	return er.OpenFile()
 }
 
 // Skip to a line number in the file. Must be called before any reading is
@@ -50,16 +102,57 @@ func (er *EveReader) Pos() (uint64) {
 	return er.lineno
 }
 
+func (er *EveReader) IsNewFile() bool {
+	fileInfo1, err := er.file.Stat()
+	if err != nil {
+		return false
+	}
+	fileInfo2, err := os.Stat(er.filename)
+	if err != nil {
+		return false
+	}
+	return !os.SameFile(fileInfo1, fileInfo2)
+}
 
 func (er *EveReader) Next() (map[string]interface{}, error) {
 
+	// Check for file truncation.
+	fileInfo, err := er.file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	if fileInfo.Size() < er.size {
+		// Truncated, seek to 0.
+		er.file.Seek(0, 0)
+		er.lineno = 0
+	}
+	er.size = fileInfo.Size()
+
 	var event map[string]interface{}
 
-	if err := er.decoder.Decode(&event); err != nil {
+	line, err := er.reader.ReadBytes('\n')
+	if err != nil {
+		if err == io.EOF {
+			// Check for rotation.
+			if er.IsNewFile() {
+				err = er.Reopen()
+				if err != nil {
+					return nil, err
+				}
+				return er.Next()
+			}
+		}
 		return nil, err
 	}
 
 	er.lineno++
+
+	decoder := json.NewDecoder(bytes.NewReader(line))
+	decoder.UseNumber()
+
+	if err := decoder.Decode(&event); err != nil {
+		return nil, err
+	}
 
 	return event, nil
 }
